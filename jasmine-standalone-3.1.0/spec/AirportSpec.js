77aab0d36aa67ca72a7be6bff82d70f1694e28c9
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-15 14:03:18 +0100

    Add Airport.land method

--- jasmine-standalone-3.1.0/spec/AirportSpec.js
+++ jasmine-standalone-3.1.0/spec/AirportSpec.js
@@ -1,3 +1,4 @@
+
 describe("Airport.planes", function() {
   it("Should be an empty array", function() {
     airport = new Airport
@@ -8,6 +9,15 @@ describe("Airport.planes", function() {
 describe("Airport.capacity", function() {
   it("Should be 55", function() {
     airport = new Airport
-    expect(airport.capacity).toEqual(55)
+    expect(airport.capacity).toEqual(55);
+  });
+});
+
+describe("Airport.land", function() {
+  it("Should add a plane to planes array", function() {
+    airport = new Airport
+    plane = {}
+    airport.land(plane)
+    expect(airport.planes).toEqual([plane]);
   });
 });
--- jasmine-standalone-3.1.0/src/airport.js
+++ jasmine-standalone-3.1.0/src/airport.js
@@ -3,4 +3,8 @@ const AIRPORT_CAPACITY = 55;
 function Airport() {
   this.planes = [];
   this.capacity = AIRPORT_CAPACITY;
-}
+};
+
+Airport.prototype.land = function(plane) {
+  this.planes.push(plane);
+};
