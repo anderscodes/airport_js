603bddae95b6288c934c3327785c53698dcd6449
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-15 16:39:05 +0100

    Refactor tests

--- jasmine-standalone-3.1.0/spec/AirportSpec.js
+++ jasmine-standalone-3.1.0/spec/AirportSpec.js
@@ -1,41 +1,36 @@
+plane = jasmine.createSpy('plane');
+beforeEach(function() {
+  airport = new Airport();
+  const AIRPORT_CAPACITY = 55;
+});
 
 describe("Airport.planes", function() {
   it("Should be an empty array", function() {
-    airport = new Airport
     expect(airport.planes).toEqual([]);
   });
 });
 
 describe("Airport.capacity", function() {
   it("Should be 55", function() {
-    airport = new Airport
-    expect(airport.capacity).toEqual(55);
+    expect(airport.capacity).toEqual(AIRPORT_CAPACITY);
   });
 });
 
 describe("Airport.land", function() {
   it("Should add a plane to planes array", function() {
-    airport = new Airport
-    plane = {}
     airport.land(plane)
     expect(airport.planes).toEqual([plane]);
   });
   it("should raise an error if the airport is full", function(){
-    airport = new Airport
-    times = 55
-    plane = {}
-    for(var i=0; i<times; i++) {
+    for(var i=0; i<AIRPORT_CAPACITY; i++) {
     airport.land(plane);
     }
-    console.log(airport.planes)
     expect(function() {airport.land(plane);}).toThrow("Airport is FULL!");
-  })
+  });
 });
 
 describe("Airport.takeOff", function() {
   it("Should remove the selected plane from the planes array", function(){
-    airport = new Airport
-    plane = {}
     airport.land(plane)
     airport.takeOff(plane)
     expect(airport.planes).toEqual([]);
@@ -44,7 +39,6 @@ describe("Airport.takeOff", function() {
 
 describe("Airport.takeOff", function() {
   it("Should remove the selected plane from the planes array", function(){
-    airport = new Airport
     plane1 = 'plane1'
     plane2 = 'plane2'
     plane3 = 'plane3'
@@ -58,14 +52,10 @@ describe("Airport.takeOff", function() {
 
 describe("Airport.isplaneInAirport", function() {
   it("Should return true if plane is in airport", function() {
-    airport = new Airport
-    plane = 'plane'
     airport.land(plane)
     expect(airport.isplaneInAirport(plane)).toBe(true)
   });
   it("should return false if plane is not in airport", function() {
-    airport = new Airport
-    plane = 'plane'
     airport.land(plane)
     airport.takeOff(plane)
     expect(airport.isplaneInAirport(plane)).toBe(false)
